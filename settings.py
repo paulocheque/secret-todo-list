e0eaa212ad95a38d4a68dccbeb62fc35f1bbf911
paulocheque <paulocheque@gmail.com>
2013-09-27 13:40:29 -0300

    Using environment variable to set secret variables

--- settings.py
+++ settings.py
@@ -6,7 +6,8 @@ DEBUG = False
 if DEBUG:
     DOMAIN = 'localhost:5000'
 else:
-    DOMAIN = 'secret-todo-list.herokuapp.com'
+    DOMAIN = os.environ.get('APP_DOMAIN', 'secret-todo-list.herokuapp.com')
+
 
 TORNADO_SETTINGS = dict(
     gzip=True,
@@ -18,18 +19,18 @@ TORNADO_SETTINGS = dict(
     autoescape=None,
     cookie_secret=os.environ.get('BSALT', 'your salt'),
 
-    google_consumer_key='',
-    google_consumer_secret='',
+    google_consumer_key=os.environ.get('GOOGLE_CONSUMER_KEY', ''),
+    google_consumer_secret=os.environ.get('GOOGLE_CONSUMER_SECRET', ''),
 
     facebook_redirect_uri='http://%s/auth/facebook' % DOMAIN,
-    facebook_api_key='',
-    facebook_secret='',
+    facebook_api_key=os.environ.get('FACEBOOK_API_KEY', ''),
+    facebook_secret=os.environ.get('FACEBOOK_SECRET', '')
 
     github_redirect_uri='http://%s/auth/github' % DOMAIN,
-    github_client_id='',
-    github_secret='',
-    github_scope='',
+    github_client_id=os.environ.get('GITHUB_CLIENT_ID', ''),
+    github_secret=os.environ.get('GITHUB_SECRET', ''),
+    github_scope=os.environ.get('GITHUB_SCOPE', ''),
 
-    twitter_consumer_key='',
-    twitter_consumer_secret='',
+    twitter_consumer_key=os.environ.get('TWITTER_CONSUMER_KEY', ''),
+    twitter_consumer_secret=os.environ.get('TWITTER_CONSUMER_SECRET', ''),
 )
